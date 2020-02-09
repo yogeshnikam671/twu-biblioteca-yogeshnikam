f5db43e86e5439264f6d998d0b0942edbd01f640
Yogesh Nikam <yogesh.nikam@thoughtworks.com>
2020-02-09 23:51:30 +0530

    Refactor. Remove the warning regarding while(true) in BibliotecaApp class

--- src/main/java/com/twu/biblioteca/BibliotecaApp.java
+++ src/main/java/com/twu/biblioteca/BibliotecaApp.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class BibliotecaApp {
 
-
+    @SuppressWarnings("InfiniteLoopStatement")
     public static void main(String[] args) {
         Librarian librarian = new Librarian();
         Library library = new Library(librarian);
